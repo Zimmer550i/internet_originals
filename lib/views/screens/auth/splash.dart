217284bf9d6013744853062505cb046fc718761a
Zimmer550i <sakif049@gmail.com>
2025-04-21 18:30:01 +0600

    fix: remove firebase

--- lib/views/screens/auth/splash.dart
+++ lib/views/screens/auth/splash.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:internet_originals/views/screens/auth/onboarding.dart';
@@ -18,30 +17,11 @@ class _SplashState extends State<Splash> {
   @override
   void initState() {
     super.initState();
-    // _getFCMToken();
     WidgetsBinding.instance.addPostFrameCallback((duration) {
       Future.delayed(Duration(milliseconds: 1200), () {
         Get.off(() => Onboarding(), transition: Transition.noTransition);
       });
     });
-  }
-
-  // ignore: unused_element
-  Future<void> _getFCMToken() async {
-    String? fcmToken;
-    FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-    String? token = await messaging.getToken();
-
-    setState(() {
-      if (token != null) {
-        fcmToken = token;
-      } else {
-        fcmToken = "Failed to get token";
-      }
-    });
-
-    debugPrint("FCM Token: $fcmToken");
   }
 
   @override
